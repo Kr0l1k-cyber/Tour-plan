3fc09340420ef25805f2205c48e12be1b5d8bf6b
Kr0l1k-cyber <CoolSpot64rus@gmail.com>
2022-06-14 21:32:52 +0400

    Прикрутил API Яндекс карт

--- Js/main.js
+++ Js/main.js
@@ -7,3 +7,11 @@ var swiper = new Swiper('.swiper-container', {
     prevEl: '.slider-button--prev',
   },
 });
+//карты яндекса
+function init(){
+  var map = new ymaps.Map('map', {
+  center: [36.13990039624475,-115.16105149619955],
+  zoom: [10], 
+  });
+};
+ymaps.ready(init);
