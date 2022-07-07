7944b7a1e80dd7699ff459213a55fdf47ced5756
Kr0l1k-cyber <CoolSpot64rus@gmail.com>
2022-07-07 10:42:54 +0400

    Сделал mobie menu

--- Js/main.js
+++ Js/main.js
@@ -1,29 +1,38 @@
-var hotelSlider = new Swiper('.hotel-slider', {
+var hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
   },
 });
 //карты яндекса
-function init(){
-  var map = new ymaps.Map('map', {
-  center: [36.13990039624475,-115.16105149619955],
-  zoom: [10], 
+function init() {
+  var map = new ymaps.Map("map", {
+    center: [36.13990039624475, -115.16105149619955],
+    zoom: [10],
   });
-};
+}
 ymaps.ready(init);
 // Слайдер с отзывами
-var reviewsSlider = new Swiper('.reviews-slider', {
+var reviewsSlider = new Swiper(".reviews-slider", {
   centeredSlides: true,
   slidesPerView: 0.5,
   // 2 пункта выше добавил что бы скрыть лишние слайды при адаптиве 768px
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
   },
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик по кнопке меню");
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+});
+// всплывающее меню 756px<
