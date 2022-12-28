57c7fb89702ef00da707959210b7714a32c8df21
Kr0l1k-cyber <CoolSpot64rus@gmail.com>
2022-12-28 20:10:36 +0300

    Валидация

--- Js/main.js
+++ Js/main.js
@@ -59,4 +59,19 @@ $(document).ready(function () {
     modalDialog.removeClass("modal__dialog--visible");
   }
   // удаляет класс закрывает модальное окно
+  // ------------------------------------------------------------------------------------------
+  $('.modal__form').validate({
+    messages: {
+      name: "Please specify your name",
+      email: {
+        required: "We need your email address to contact you",
+        email: "Your email address must be in the format of name@domain.com"
+      },
+      phone: {
+        required: "Телефон обязателен",
+      },
+    }
+  
+  });
+  // обработка формы
 });
